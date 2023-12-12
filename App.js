f2e00d9d72240d48c22bef412f7a0ad12d846ef6
Omprakash <opamarwal7@gmail.com>
2023-12-12 17:19:45 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,6 @@
+const header = React.createElement("h1",{id:"header"},"My First React Program in React");
+console.log(header);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+
+root.render(header);
